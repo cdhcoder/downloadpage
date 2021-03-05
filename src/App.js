0846dcd32ded69f8942e99df1c5edcef191128de
cdhcoder <dahyun0331@gmail.com>
2021-03-05 06:06:12 +0000

    Initialize

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Notice from './Notice';
+
+function App() {
+  return (
+    <div>
+      <Notice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Notice.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Notice() {
+    return (
+        <div>하이</div>
+    );
+}
+
+export default Notice;
